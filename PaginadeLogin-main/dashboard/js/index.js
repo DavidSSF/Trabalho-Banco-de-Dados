6d79c8f1b1a98f788623abc97d62786e21c573c5
Anderson Melo da Silva <111611117+0AndersonMelo0@users.noreply.github.com>
2023-10-21 15:46:23 -0300

    Add files via upload

---
+++ PaginadeLogin-main/dashboard/js/index.js
@@ -0,0 +1,34 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+let loc = localStorage.getItem('tema');
+if(loc == 'bleck'){
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+}
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+    
+    let loc = localStorage.getItem('tema');
+    if(loc == 'bleck'){
+        localStorage.setItem('tema','wite')
+    }else{
+        localStorage.setItem('tema','bleck')
+    }
+})
